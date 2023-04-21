af6d53f17982cd953f5e47ee4faa96e01c3868d6
MD KAMRAN KHAN <124767601+kamrankhan0001@users.noreply.github.com>
2023-04-21 23:26:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function abc["https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80"
+			"https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80"
+	"https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80"
+		"https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80"]
+
+
+const button= document.getElementById("download-images-button");
+button.addEventListener('On click' ,()=>{
+	.then(images => {
+      images.forEach(image => {
+        const img = document.createElement('img');
+})
+	promise.all("download"){
+	.then(responce=>{
+	document.getElementById("download-images-button");
+	
+	})
+		  .catch(eroor=>console.error(Failed to load image's URL: ${image.url));
+	
+	})
+}
